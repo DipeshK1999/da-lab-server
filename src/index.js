592da39ed97373dd4c553e3e2d3b29bfb3705591
DipeshK1999 <148889685+DipeshK1999@users.noreply.github.com>
2024-03-27 16:14:38 +0100

    config udpate

--- src/index.js
+++ src/index.js
@@ -2,24 +2,23 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import mysql from "mysql";
 import { labDaRoutes } from "./routes/index.js";
 
-// DB Connections
-import mysql from "mysql";
-
-/*export var con = mysql.createConnection({
+export var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "6jubwe32",
-  port: 3306
-});*/
-
-export var con = mysql.createConnection({
-  host: "da-lab-db.database.windows.net",
-  user: "da-db-root",
-  password: "6jubwe32X",
-  port: 1433,
+  port: 3306,
 });
+
+// export var con = mysql.createConnection({
+//   host: "da-lab-server.database.windows.net",
+//   user: "da-db-root",
+//   password: "",
+//   port: 1433,
+//   database: "lab_da",
+// });
 
 con.connect(function (err) {
   if (err) throw err;
